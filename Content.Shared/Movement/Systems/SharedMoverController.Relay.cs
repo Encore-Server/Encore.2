cdb7a5f168f000906f68a6f5e232f55395c976d8
EmoGarbage404 <retron404@gmail.com>
2025-09-13 01:03:00 -0400

    Create event for reacting to CanMove updates

--- Content.Shared/Movement/Systems/SharedMoverController.Relay.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.Relay.cs
@@ -1,4 +1,5 @@
 using Content.Shared.Movement.Components;
+using Content.Shared.Movement.Events;
 
 namespace Content.Shared.Movement.Systems;
 
@@ -10,6 +11,7 @@ public abstract partial class SharedMoverController
         SubscribeLocalEvent<MovementRelayTargetComponent, ComponentShutdown>(OnTargetRelayShutdown);
         SubscribeLocalEvent<MovementRelayTargetComponent, AfterAutoHandleStateEvent>(OnAfterRelayTargetState);
         SubscribeLocalEvent<RelayInputMoverComponent, AfterAutoHandleStateEvent>(OnAfterRelayState);
+        SubscribeLocalEvent<RelayInputMoverComponent, CanMoveUpdatedEvent>(OnRelayCanMoveUpdated);
     }
 
     private void OnAfterRelayTargetState(Entity<MovementRelayTargetComponent> entity, ref AfterAutoHandleStateEvent args)
@@ -20,6 +22,11 @@ public abstract partial class SharedMoverController
     private void OnAfterRelayState(Entity<RelayInputMoverComponent> entity, ref AfterAutoHandleStateEvent args)
     {
         Physics.UpdateIsPredicted(entity.Owner);
+    }
+
+    private void OnRelayCanMoveUpdated(Entity<RelayInputMoverComponent> ent, ref CanMoveUpdatedEvent args)
+    {
+        Log.Debug("Updated CanMove!");
     }
 
     /// <summary>
